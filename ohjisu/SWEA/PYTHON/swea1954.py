da6e799870dfd47b088c70526c44f34bbf8ae62d
ohjisu320 <ohjisu320@gmail.com>
2025-08-08 01:34:13 +0900

    fix(SWEA1954 / D2): 달팽이 숫자

--- ohjisu/SWEA/PYTHON/swea1954.py
+++ ohjisu/SWEA/PYTHON/swea1954.py
@@ -6,169 +6,124 @@
 달팽이의 크기 N은 1 이상 10 이하의 정수이다. (1 ≤ N ≤ 10)
 
 """
+from pprint import pprint
+
+T = int(input()) 
+for tc in range(1, T+1) :
+    N = int(input())
+    arr = [[0] * N for _ in range(N)]
+    idx = N
+    i = 0
+    j = N-1
+
+    arr[0] = [i+1 for i in range(N)]
+
+    def recursive(size, idx, i, j, loop) :
+        di = [1, 0, -1, 0] # arr[0]을 주고 시작하기 때문에
+        dj = [0, -1, 0, 1] # 방향은 하 좌 상 우 순으로 진행 
+        # N-1 * 2번 반복 -> N-2 * 2번 반복 -> ... 1 * 2번 반복
+        for _ in range(2) :         # 2번 반복
+            for _ in range(size):   # size == (N-1)만큼 반복
+                i += di[(loop%4)] 
+                j += dj[(loop%4)]
+                idx += 1
+                arr[i][j] = idx
+            loop += 1               # 작은 for문 끝난 후 방향 바꾸기
+                
+        if idx == N**2 :            # idx가 N*N일 때 끝남
+            return 
+        recursive(size-1, idx, i, j, loop)  # 안끝난다면 재귀
+        
+    recursive(N-1, idx, i, j, 0)
+    print(f'#{tc}')
+    for a in arr : 
+        print(*a) 
+        
+
 
 """
-기본 -> x 순회
-for x in range(N)
-if x == N-1 :
-    for y in range(N)
-        ..
-       
-
-"""
-"""
-인덱스: 0 ~ N까지 --
-넣을 값: 1, N+1
+while 문 사용 --> 불필요해서 삭제
 """
 
 # N = int(input())
-N = 3
-arr = [[0] * N for _ in range(N)]
-idx = N
-x = N-1
-y = 0
+# arr = [[0] * N for _ in range(N)]
+# idx = N
+# i = 0
+# j = N-1
 
-arr[0] = [i+1 for i in range(N)]
+# arr[0] = [i+1 for i in range(N)]
 
-  
+# # arr = 
 
-def func(idx, arr, x, y) :
-    if x == N//2 + 1 and  y == N//2 + 1 :
-        return arr
-    di = [0, -1, 0, 1] # 우 하 좌 상
-    dj = [1, 0, -1, 0] 
-    # 조건 걸어서 방향 설정 
-    # x == N-1, y == N-1
-    # -> 왼쪽으로
-    if x == N-1 and y == N-1 : ## N 이 변동값이 아님
-        x += di[2]
-        y += dj[2]
-    # x == N-1, y == 0
-    # -> 위로
-    elif x == N-1 and y == 0 :
-        x += di[3]
-        y += dj[3]
+# def recursive(size, idx, i, j, loop) :
+#     # size가 1일 때까지 도는 while문
+
+#     di = [1, 0, -1, 0] # arr[0]을 주고 시작하기 때문에
+#     dj = [0, -1, 0, 1] # 방향은 하 좌 상 우 순으로 진행 
+#     # 조건 걸어서 방향 설정 
+#     # range_num = 1 if size == 1 else 2
+#     for count in range(2) :
+#         if size % 2 == 0 :
+#             i += di[loop%4] 
+#             j += dj[loop%4]  
         
-    # x == 0, y==N-1
-    # -> 아래로
-    elif y == N-1 :
+#         idx += 1
         
-        x += di[1]
-        y += dj[1]
+#         # if count % 2 == 0 and arr[i][j] == 0 :
+#         arr[i][j] = idx
+        
+#         if 
+#             return recursive(size-1, idx, i, j, loop+1)
+
+#     # if (size == 0) : # 조건 수정 (arr[i+di[(loop+1)%4]][j+dj[(loop+1)%4]] == 0)
+#     #     return recursive(size+1, idx, i, j, loop)
+#     if idx == N**2 :
+#         return 
     
+# recursive(N-1, idx, i, j, 0)
+# pprint(arr)
+
+"""
+x와 y의 값 조건대로 if문 구성 -- 실패
+"""
+# def func(N, idx, arr, i, j) :
+#     if N == 0  :
+#         return arr
+    
+            
+#             x += 1 # di[3]
+#             y += 0 # dj[3]
+        
+
     # x == 0, y == 0
     # -> 오른쪽으로
-    elif y >= 0 :
-        x += di[0]
-        y += dj[0]
+    # if y >= 0 :
+    #     x += di[0]
+    #     y += dj[0]
 
-    idx += 1
-    arr[x][y] = idx
-    
-    return func(idx, arr, x, y)
+    # # x == N-1, y == N-1
+    # # -> 왼쪽으로
+    # if x == N-1 and y == N-1 : ## N 이 변동값이 아님
+    #     x += di[2]
+    #     y += dj[2]
+    # # x == N-1, y == 0
+    # # -> 위로
+    # elif x == N-1 and y == 0 : # 딕셔너리 만들어서 up : [x == N-1, y ==o] 형태로 컨디션 담아 놓
+    #     x += di[3]
+#     #     y += dj[3]
+        
 
-func(idx, arr, 1, 1)
-
-
-
-                    
-
-
-
-print(arr)  
-
-
-
-# if x == N-1 :
-#     for y_1 in range(1, N) :
-#         idx += 1
-#         arr[y_1][x] = idx
-#         if y_1 == N-1 :
-#             for x_1 in range(N-2, -1, -1) :
-#                 idx += 1
-#                 arr[y_1][x_1] = idx
-#                 if x_1 == 0 :
-#                     for x_2 in range(0, N-1) :
-#                         idx += 1
-#                         arr[y_1-1][x_1] = idx
-#                     break
-
-
-# for y in range(0, N) :
-#     for x in range(0, N) :
-#         idx += 1
-#         arr[y][x] = idx
-#         if x == N-1 :
-#             for y_1 in range(1, N) :
-                
-#                 idx += 1
-#                 arr[y_1][x] = idx
-#                 if y_1 == N-1 :
-#                     for x_1 in range(N-2, -1, -1) :
-#                         idx += 1
-#                         arr[y_1][x_1] = idx
-#                         if x_1 == 0 :
-#                             for y_2 in range(2, N-1) :
-#                                 idx += 1
-#                                 arr[y_2][x_1] = idx
-#                                 if y_2 == N-1 :
-
-                                
-#                                     print(arr)
-
-# arr[0] = [i+1 for i in range(N)]
-
-# def loops_x(y, idx, start, stop) :
-#     for x in range(start, stop) :
-#         idx += 1
-#         arr[x][y] = idx
-#     return arr
-
-# def loops_y(x, idx, start, stop) :
-#     for y in range(start, stop) :
-#         idx += 1
-#         arr[x][y] = idx
-#     return arr
-
-# # for y in range(N) :
-# #     for x in range(N) :
-# #         idx += 1
-# #         arr[y][x] = idx 
-# x = N-1
-# y = 0
-# if x == N-1 :
-#     arr = loops_y(x, idx, 1, N-1)
-# if y == N-1 :
-#     arr = loops_x(y, idx, N-1, -1)
-# print(arr)
-
-
-
-
-
-# def loops(arr, N) :
-#     global idx
 #     idx += 1
 #     arr[x][y] = idx
-#     if x == N-1 :
-#         return loops(arr[1:], x)
-#     if y == N-1 :
-#         return loops(arr[1:], y)
-#     return  arr
+#     # N = N-1
+#     return func(N, idx, arr, x, y)
 
-# idx = 0
-# arr[0] = [i+1 for i in range(N)]
+# func(N, idx, arr, 0, 2)                  
+# print(arr)  
 
-# def loops(arr, start, stop, step) :
-#     global idx
-#     for item in range(start,  stop, step) :
-#         idx += 1
-#         arr[x][y] = idx
-#         if x == stop-1 :
-#             loops(arr, )
-
-# loops(arr[0], 0, )
-
+"""
+규칙 찾는 중
+"""
 
 # x = 0
 # y = 0 
