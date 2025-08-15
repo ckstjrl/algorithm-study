e37051adc26234dfd573fcb3fbb49b5e431a19d8
aeongseo <ae.ong.seo@gmail.com>
2025-08-15 13:20:35 +0900

    fix(SWEA1983 / D2) : 조교의 성적 매기기(python)

--- aeongseo/SWEA/PYTHON/swea1983.py
+++ aeongseo/SWEA/PYTHON/swea1983.py
@@ -13,7 +13,7 @@ for tc in range(1, T+1):
         m, f, h = map(int, input().split())
         total_score[i] = 0.35*m + 0.45*f + 0.2*h    # 입력 받는대로 총 점수 계산
 
-    sorted_score = sorted(total_score, reverse=True)    # 총 점수를 오름차순으로 정렬
+    sorted_score = sorted(total_score, reverse=True)    # 총 점수를 내림차순으로 정렬
 
     for j in range(N):
         if total_score[K-1] == sorted_score[j]:    # K번째 학생의 점수와 성적순 리스트의 점수가 같다면 인덱스(등수) 저장
